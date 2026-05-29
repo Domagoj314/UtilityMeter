34ce06115aa98cd1a97e22fcd43d17bfef051445
dvlahek23 <domag@LAPTOP>
2026-05-29 18:07:27 +0200

    Added tesseract ocr with test reading picture with postman

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
 from flask import Flask, request
 import numpy as np
 import cv2
+import pytesseract as tess
+from PIL import Image
+from secrets import PATH_TO_TESSERACT_EXECUTABLE
+
+tess.pytesseract.tesseract_cmd = PATH_TO_TESSERACT_EXECUTABLE
 
 app = Flask(__name__)
 
@@ -9,11 +14,13 @@ def ocr():
     jpg = request.data
     arr = np.frombuffer(jpg, dtype=np.uint8)
     img = cv2.imdecode(arr, cv2.IMREAD_COLOR)
-    img = cv2.rotate(img, cv2.ROTATE_180)
-    img = cv2.flip(img, 1)
     cv2.imwrite("picture.png", img)
     print("Picture saved!")
-    return "OK", 200
+    image = Image.open("picture.png")
+    text = tess.image_to_string(image)
+    print(text)
+    return text, 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
